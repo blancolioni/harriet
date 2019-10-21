1bec0731bab64a14c8cd30b6fe02eebc0ac603c6
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-10-21 18:29:31 +0200

    expanded package Harriet.Commodities

--- src/commodities/harriet-commodities.adb
+++ src/commodities/harriet-commodities.adb
@@ -1,5 +1,7 @@
-with Harriet.Db.Stock_Item;
 with Harriet.Db.Commodity;
+with Harriet.Db.Input_Commodity;
+with Harriet.Db.Manufactured;
+with Harriet.Db.Stock_Item;
 
 package body Harriet.Commodities is
 
@@ -29,6 +31,67 @@ package body Harriet.Commodities is
       end if;
    end Add_Stock;
 
+   ---------------
+   -- Add_Stock --
+   ---------------
+
+   procedure Add_Stock
+     (To        : in out Stock_Type;
+      Commodity : Harriet.Db.Commodity_Reference;
+      Quantity  : Harriet.Quantities.Quantity_Type)
+   is
+      use type Harriet.Quantities.Quantity_Type;
+      Tag : constant String := Harriet.Db.Commodity.Get (Commodity).Tag;
+   begin
+      if To.Contains (Tag) then
+         declare
+            Q : Harriet.Quantities.Quantity_Type renames To (Tag);
+         begin
+            Q := Q + Quantity;
+         end;
+      else
+         To.Insert (Tag, Quantity);
+      end if;
+   end Add_Stock;
+
+   ----------------------
+   -- Current_Quantity --
+   ----------------------
+
+   function Current_Quantity
+     (Of_Stock  : Harriet.Db.Has_Stock_Reference;
+      Commodity : Harriet.Db.Commodity_Reference)
+      return Harriet.Quantities.Quantity_Type
+   is
+      Stock_Item : constant Harriet.Db.Stock_Item.Stock_Item_Type :=
+        Harriet.Db.Stock_Item.Get_By_Stock_Item
+          (Of_Stock, Commodity);
+   begin
+      if Stock_Item.Has_Element then
+         return Stock_Item.Quantity;
+      else
+         return Harriet.Quantities.Zero;
+      end if;
+   end Current_Quantity;
+
+   ----------------------
+   -- Current_Quantity --
+   ----------------------
+
+   function Current_Quantity
+     (Of_Stock  : Stock_Type;
+      Commodity : Harriet.Db.Commodity_Reference)
+      return Harriet.Quantities.Quantity_Type
+   is
+      Tag : constant String := Harriet.Db.Commodity.Get (Commodity).Tag;
+   begin
+      if Of_Stock.Contains (Tag) then
+         return Of_Stock.Element (Tag);
+      else
+         return Harriet.Quantities.Zero;
+      end if;
+   end Current_Quantity;
+
    ------------
    -- Exists --
    ------------
@@ -49,13 +112,103 @@ package body Harriet.Commodities is
       return Harriet.Db.Commodity.Get_Reference_By_Tag (Tag);
    end Get;
 
+   ---------------------
+   -- Is_Manufactured --
+   ---------------------
+
+   function Is_Manufactured
+     (Commodity : Commodity_Reference)
+      return Boolean
+   is
+      use Harriet.Db;
+   begin
+      return Harriet.Db.Commodity.Get (Commodity)
+        .Top_Record = R_Manufactured;
+   end Is_Manufactured;
+
+   -----------------
+   -- Is_Resource --
+   -----------------
+
+   function Is_Resource
+     (Commodity : Commodity_Reference)
+      return Boolean
+   is
+      use Harriet.Db;
+   begin
+      return Harriet.Db.Commodity.Get (Commodity)
+        .Top_Record = R_Resource;
+   end Is_Resource;
+
    -------------------
    -- Raw_Resources --
    -------------------
 
    function Raw_Resources return Commodity_Reference is
    begin
-      return Harriet.Db.Commodity.Get_Reference_By_Tag ("raw-resource");
+      return Harriet.Db.Commodity.Get_Reference_By_Tag ("raw-resources");
    end Raw_Resources;
+
+   ------------------
+   -- Remove_Stock --
+   ------------------
+
+   procedure Remove_Stock
+     (From      : Harriet.Db.Has_Stock_Reference;
+      Commodity : Harriet.Db.Commodity_Reference;
+      Quantity  : Harriet.Quantities.Quantity_Type)
+   is
+      use Harriet.Quantities;
+      Stock : constant Harriet.Db.Stock_Item.Stock_Item_Type :=
+        Harriet.Db.Stock_Item.Get_By_Stock_Item (From, Commodity);
+   begin
+      pragma Assert (Stock.Has_Element or else Quantity = Zero);
+      pragma Assert (Quantity = Zero or else Quantity <= Stock.Quantity);
+      if Quantity > Zero then
+         Harriet.Db.Stock_Item.Update_Stock_Item
+           (Stock.Get_Stock_Item_Reference)
+           .Set_Quantity (Stock.Quantity - Quantity)
+           .Done;
+      end if;
+   end Remove_Stock;
+
+   ----------
+   -- Scan --
+   ----------
+
+   procedure Scan
+     (Stock   : Stock_Type;
+      Process : not null access
+        procedure (Commodity : Harriet.Db.Commodity_Reference;
+                   Quantity  : Harriet.Quantities.Quantity_Type))
+   is
+   begin
+      for Position in Stock.Iterate loop
+         Process
+           (Harriet.Db.Commodity.Get_Reference_By_Tag
+              (Stock_Maps.Key (Position)),
+            Stock_Maps.Element (Position));
+      end loop;
+   end Scan;
+
+   ----------------------
+   -- Scan_Ingredients --
+   ----------------------
+
+   procedure Scan_Ingredients
+     (Commodity : Commodity_Reference;
+      Process   : not null access
+        procedure (Ingredient : Commodity_Reference;
+                   Quantity   : Harriet.Quantities.Quantity_Type))
+   is
+   begin
+      for Input of
+        Harriet.Db.Input_Commodity.Select_By_Manufactured
+          (Harriet.Db.Manufactured.Get_Manufactured (Commodity)
+           .Get_Manufactured_Reference)
+      loop
+         Process (Input.Commodity, Input.Quantity);
+      end loop;
+   end Scan_Ingredients;
 
 end Harriet.Commodities;
--- src/commodities/harriet-commodities.ads
+++ src/commodities/harriet-commodities.ads
@@ -1,3 +1,5 @@
+private with WL.String_Maps;
+
 with Harriet.Quantities;
 
 with Harriet.Db;
@@ -6,15 +8,68 @@ package Harriet.Commodities is
 
    subtype Commodity_Reference is Harriet.Db.Commodity_Reference;
 
+   type Array_Of_Commodities is
+     array (Positive range <>) of Harriet.Db.Commodity_Reference;
+
    function Exists (Tag : String) return Boolean;
    function Get (Tag : String) return Commodity_Reference
      with Pre => Exists (Tag);
 
+   function Is_Resource
+     (Commodity : Commodity_Reference)
+      return Boolean;
+
+   function Is_Manufactured
+     (Commodity : Commodity_Reference)
+      return Boolean;
+
+   procedure Scan_Ingredients
+     (Commodity : Commodity_Reference;
+      Process   : not null access
+        procedure (Ingredient : Commodity_Reference;
+                   Quantity   : Harriet.Quantities.Quantity_Type));
+
    function Raw_Resources return Commodity_Reference;
+
+   function Current_Quantity
+     (Of_Stock  : Harriet.Db.Has_Stock_Reference;
+      Commodity : Harriet.Db.Commodity_Reference)
+      return Harriet.Quantities.Quantity_Type;
 
    procedure Add_Stock
      (To        : Harriet.Db.Has_Stock_Reference;
       Commodity : Harriet.Db.Commodity_Reference;
       Quantity  : Harriet.Quantities.Quantity_Type);
+
+   procedure Remove_Stock
+     (From      : Harriet.Db.Has_Stock_Reference;
+      Commodity : Harriet.Db.Commodity_Reference;
+      Quantity  : Harriet.Quantities.Quantity_Type);
+
+   type Stock_Type is private;
+
+   function Current_Quantity
+     (Of_Stock  : Stock_Type;
+      Commodity : Harriet.Db.Commodity_Reference)
+      return Harriet.Quantities.Quantity_Type;
+
+   procedure Add_Stock
+     (To        : in out Stock_Type;
+      Commodity : Harriet.Db.Commodity_Reference;
+      Quantity  : Harriet.Quantities.Quantity_Type);
+
+   procedure Scan
+     (Stock : Stock_Type;
+      Process : not null access
+        procedure (Commodity : Harriet.Db.Commodity_Reference;
+                   Quantity  : Harriet.Quantities.Quantity_Type));
+
+private
+
+   package Stock_Maps is
+     new WL.String_Maps (Harriet.Quantities.Quantity_Type,
+                         Harriet.Quantities."=");
+
+   type Stock_Type is new Stock_Maps.Map with null record;
 
 end Harriet.Commodities;
