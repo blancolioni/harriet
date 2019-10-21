66cebb47c135a933adcc6a111c132d1f0e1ee2c8
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-10-21 18:30:43 +0200

    Harriet.Installations.Updates: strip mining

--- src/installations/harriet-installations-updates.adb
+++ src/installations/harriet-installations-updates.adb
@@ -40,9 +40,13 @@ package body Harriet.Installations.Updates is
                   Harriet.Db.World.Get (Installation.World);
       Colony   : constant Harriet.Db.Colony.Colony_Type :=
                    Harriet.Db.Colony.Get (Installation.Colony);
+      Stock        : constant Harriet.Db.Has_Stock_Reference :=
+        Colony.Get_Has_Stock_Reference;
       Quantity      : Harriet.Quantities.Quantity_Type;
    begin
-      if Facility.Mining > 0 then
+      if Facility.Mining > 0
+        and then Installation.Resource /= Null_Resource_Reference
+      then
          Harriet.Worlds.Updates.Mine_Resource
            (World         => Installation.World,
             Resource      => Installation.Resource,
@@ -50,26 +54,63 @@ package body Harriet.Installations.Updates is
               Real (Facility.Mining) * Installation.Efficiency,
             Mined         => Quantity);
 
-         if Installation.Resource = Null_Resource_Reference then
-            Harriet.Logging.Log
-              (Facility.Tag & " on " & World.Name,
-               "strip-mined " & Harriet.Quantities.Show (Quantity)
-               & " raw resources");
-         else
-            Harriet.Logging.Log
-              (Facility.Tag & " on " & World.Name,
-               "mined " & Harriet.Quantities.Show (Quantity)
-               & " " & Harriet.Db.Resource.Get (Installation.Resource).Tag);
-         end if;
+         Harriet.Logging.Log
+           (Facility.Tag & " on " & World.Name,
+            "mined " & Harriet.Quantities.Show (Quantity)
+            & " " & Harriet.Db.Resource.Get (Installation.Resource).Tag);
 
          Harriet.Commodities.Add_Stock
-           (To        => Colony.Get_Has_Stock_Reference,
+           (To        => Stock,
             Commodity =>
-              (if Installation.Resource = Null_Resource_Reference
-               then Harriet.Commodities.Raw_Resources
-               else Harriet.Db.Resource.Get (Installation.Resource)
-               .Get_Commodity_Reference),
+              Harriet.Db.Resource.Get (Installation.Resource)
+            .Get_Commodity_Reference,
             Quantity  => Quantity);
+      end if;
+
+      if Facility.Strip_Mining > 0 then
+         Harriet.Worlds.Updates.Mine_Resource
+           (World         => Installation.World,
+            Resource      => Harriet.Db.Null_Resource_Reference,
+            Effectiveness =>
+              Real (Facility.Strip_Mining) * Installation.Efficiency,
+            Mined         => Quantity);
+
+         Harriet.Logging.Log
+           (Facility.Tag & " on " & World.Name,
+            "strip-mined " & Harriet.Quantities.Show (Quantity)
+            & " raw resources");
+
+         Harriet.Commodities.Add_Stock
+           (To        => Stock,
+            Commodity => Harriet.Commodities.Raw_Resources,
+            Quantity  => Quantity);
+      end if;
+
+      if Facility.Industry > 0 then
+         if Installation.Resource /= Null_Resource_Reference then
+            declare
+               use Harriet.Quantities;
+               Converted : constant Quantity_Type :=
+                 Min (To_Quantity (Real (Facility.Industry)),
+                      Harriet.Commodities.Current_Quantity
+                        (Stock, Harriet.Commodities.Raw_Resources));
+            begin
+               Harriet.Commodities.Remove_Stock
+                 (Stock, Harriet.Commodities.Raw_Resources, Converted);
+               Harriet.Commodities.Add_Stock
+                 (Stock,
+                  Harriet.Db.Resource.Get (Installation.Resource)
+                  .Get_Commodity_Reference,
+                  Scale (Converted, 0.1));
+               Harriet.Logging.Log
+                 (Facility.Tag & " on " & World.Name,
+                  "converted " & Harriet.Quantities.Show (Converted)
+                  & " raw resources to "
+                  & Harriet.Quantities.Show (Scale (Converted, 0.1))
+                  & " "
+                  & Harriet.Db.Resource.Get (Installation.Resource).Tag);
+            end;
+         end if;
       end if;
 
       Harriet.Updates.Events.Update_With_Delay
