9a0dc2dffe31badba9c7d088cef281d14caff0db
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-10-21 09:30:04 +0200

    Refactored colony update; added historical revenue/expenses

--- src/colonies/harriet-colonies-updates.adb
+++ src/colonies/harriet-colonies-updates.adb
@@ -12,9 +12,11 @@ with Harriet.Factions;
 with Harriet.Worlds;
 
 with Harriet.Db.Colony;
+with Harriet.Db.Expense;
 with Harriet.Db.Facility;
-with Harriet.Db.Installation;
 with Harriet.Db.Faction;
+with Harriet.Db.Installation;
+with Harriet.Db.Revenue;
 with Harriet.Db.World;
 
 with Harriet.Logging;
@@ -32,24 +34,14 @@ package body Harriet.Colonies.Updates is
    overriding procedure Activate
      (Update : Colony_Update);
 
-   function Population_Update
-     (Colony  : Harriet.Db.Colony.Colony_Type;
-      World   : Harriet.Db.World.World_Type;
-      Faction : Harriet.Db.Faction.Faction_Type)
-      return Harriet.Quantities.Quantity_Type;
+   procedure Population_Update
+     (Update  : Colony_Update'Class);
 
-   function Tax_Revenue
-     (Colony  : Harriet.Db.Colony.Colony_Type;
-      World   : Harriet.Db.World.World_Type;
-      Faction : Harriet.Db.Faction.Faction_Type)
-      return Harriet.Money.Money_Type;
+   procedure Tax_Revenue
+     (Update  : Colony_Update'Class);
 
-   procedure Update_Installations
-     (Colony  : Harriet.Colonies.Colony_Handle;
-      World   : Harriet.Db.World_Reference;
-      Faction : Harriet.Factions.Faction_Type'Class;
-      Pop     : Harriet.Quantities.Quantity_Type;
-      Cash    : in out Harriet.Money.Money_Type);
+   procedure Installations_Update
+     (Update : Colony_Update'Class);
 
    --------------
    -- Activate --
@@ -58,38 +50,14 @@ package body Harriet.Colonies.Updates is
    overriding procedure Activate
      (Update : Colony_Update)
    is
-      use type Harriet.Money.Money_Type;
-      Colony  : constant Harriet.Db.Colony.Colony_Type :=
-                  Harriet.Db.Colony.Get (Update.Colony);
-      World   : constant Harriet.Db.World.World_Type :=
-                  Harriet.Db.World.Get (Colony.World);
-      Faction : constant Harriet.Db.Faction.Faction_Type :=
-                  Harriet.Db.Faction.Get (Colony.Faction);
-      New_Pop : constant Harriet.Quantities.Quantity_Type :=
-                  Population_Update (Colony, World, Faction);
-      Revenue : constant Harriet.Money.Money_Type :=
-                  Tax_Revenue (Colony, World, Faction);
-      Cash    : Harriet.Money.Money_Type :=
-                  Faction.Cash + Revenue;
    begin
 
-      Update_Installations
-        (Colony  => Get (Colony.Get_Colony_Reference),
-         World   => Colony.World,
-         Faction => Harriet.Factions.Get (Colony.Faction),
-         Pop     => New_Pop,
-         Cash    => Cash);
+      Update.Population_Update;
+      Update.Installations_Update;
+      Update.Tax_Revenue;
 
-      Harriet.Db.Colony.Update_Colony (Update.Colony)
-        .Set_Population (New_Pop)
-        .Set_Wealth (Colony.Wealth - Revenue)
-        .Done;
-
-      Harriet.Db.Faction.Update_Faction (Colony.Faction)
-        .Set_Cash (Cash)
-        .Done;
-
-      Harriet.Factions.Synchronise (Colony.Faction);
+      Harriet.Factions.Synchronise
+        (Harriet.Db.Colony.Get (Update.Colony).Faction);
 
       Harriet.Updates.Events.Update_With_Delay
         (Harriet.Calendar.Days (1), Update);
@@ -107,102 +75,25 @@ package body Harriet.Colonies.Updates is
       return Colony_Update'(Colony => Reference);
    end Daily_Update;
 
-   -----------------------
-   -- Population_Update --
-   -----------------------
+   --------------------------
+   -- Installations_Update --
+   --------------------------
 
-   function Population_Update
-     (Colony  : Harriet.Db.Colony.Colony_Type;
-      World   : Harriet.Db.World.World_Type;
-      Faction : Harriet.Db.Faction.Faction_Type)
-      return Harriet.Quantities.Quantity_Type
+   procedure Installations_Update
+     (Update : Colony_Update'Class)
    is
-      use Harriet.Real_Images;
-      Birth_Rate  : constant Unit_Real :=
-                      0.02 * World.Habitability;
-      Death_Rate  : constant Unit_Real :=
-                      (2.0 - World.Habitability) / 200.0;
-      Daily_Rate  : constant Unit_Real :=
-                      (Birth_Rate - Death_Rate) / 365.0;
-      Current_Pop : constant Non_Negative_Real :=
-                      Harriet.Quantities.To_Real (Colony.Population);
-      New_Pop     : constant Non_Negative_Real :=
-                      Current_Pop * (1.0 + Daily_Rate);
-   begin
-      Harriet.Logging.Log
-        (World.Name
-         & " owned by "
-         & Faction.Name,
-         "habitability: "
-         & Approximate_Image (World.Habitability * 100.0) & "%"
-         & "; current pop "
-         & Approximate_Image (Current_Pop)
-         & "; birth rate: "
-         & Approximate_Image (Birth_Rate * 100.0) & "%"
-         & "; death rate: "
-         & Approximate_Image (Death_Rate * 100.0) & "%"
-         & "; change: "
-         & Approximate_Image (New_Pop - Current_Pop)
-         & "; new pop: "
-         & Approximate_Image (New_Pop));
-      return Harriet.Quantities.To_Quantity (New_Pop);
-   end Population_Update;
-
-   -----------------
-   -- Tax_Revenue --
-   -----------------
-
-   function Tax_Revenue
-     (Colony  : Harriet.Db.Colony.Colony_Type;
-      World   : Harriet.Db.World.World_Type;
-      Faction : Harriet.Db.Faction.Faction_Type)
-      return Harriet.Money.Money_Type
-   is
-      use Harriet.Elementary_Functions;
-      use Harriet.Money;
-      use Harriet.Real_Images;
-      Current_Wealth : constant Money_Type := Colony.Wealth;
-      Tax_Rate       : constant Unit_Real := Colony.Tax_Rate;
-      Tax_Evasion    : constant Unit_Real := Colony.Tax_Evasion;
-      Loyalty        : constant Unit_Real := Colony.Loyalty;
-      Daily_Rate     : constant Unit_Real :=
-                         (Tax_Rate * (1.0 - Tax_Evasion) * Sqrt (Loyalty))
-                         / 365.0;
-      Revenue        : constant Money_Type :=
-                         Adjust (Current_Wealth, Daily_Rate);
-   begin
-      Harriet.Logging.Log
-        (World.Name
-         & " owned by "
-         & Faction.Name,
-         "wealth: " & Show (Current_Wealth)
-         & "; tax rate: "
-         & Approximate_Image (Tax_Rate * 100.0) & "%"
-         & "; tax evasion: "
-         & Approximate_Image (Tax_Evasion * 100.0) & "%"
-         & "; loyalty: "
-         & Approximate_Image (Loyalty * 100.0) & "%"
-         & "; daily rate: "
-         & Approximate_Image (Tax_Rate * 100.0) & "%"
-         & "; revenue: "
-         & Show (Revenue));
-      return Revenue;
-
-   end Tax_Revenue;
-
-   -----------------------
-   -- Update_Installations --
-   -----------------------
-
-   procedure Update_Installations
-     (Colony  : Harriet.Colonies.Colony_Handle;
-      World   : Harriet.Db.World_Reference;
-      Faction : Harriet.Factions.Faction_Type'Class;
-      Pop     : Harriet.Quantities.Quantity_Type;
-      Cash    : in out Harriet.Money.Money_Type)
-   is
-      pragma Unreferenced (Faction, Colony);
       use Harriet.Money, Harriet.Quantities;
+      Colony  : constant Harriet.Db.Colony.Colony_Type :=
+        Harriet.Db.Colony.Get (Update.Colony);
+      Faction : constant Harriet.Db.Faction.Faction_Type :=
+        Harriet.Db.Faction.Get (Colony.Faction);
+      World   : constant Harriet.Db.World_Reference :=
+        Colony.World;
+
+      Pop     : constant Harriet.Quantities.Quantity_Type :=
+        Colony.Population;
+      Cash    : constant Harriet.Money.Money_Type :=
+        Faction.Cash;
 
       type Installation_Record is
          record
@@ -216,7 +107,7 @@ package body Harriet.Colonies.Updates is
         new Ada.Containers.Vectors (Positive, Installation_Record);
 
       Installations   : Installation_Vectors.Vector;
-      Efficiency      : Unit_Real := 1.0;
+      Efficiency      : Unit_Real := Colony.Loyalty;
       Total_Cost      : Money_Type := Zero;
       Total_Pop       : Quantity_Type := Zero;
       Total_Power     : Natural := 0;
@@ -227,15 +118,15 @@ package body Harriet.Colonies.Updates is
 
          declare
             Reference : constant Harriet.Db.Installation_Reference :=
-                          Installation.Get_Installation_Reference;
+              Installation.Get_Installation_Reference;
             Cost      : constant Money_Type :=
-                          Harriet.Installations.Daily_Cost (Installation);
+              Harriet.Installations.Daily_Cost (Installation);
             Employees : constant Quantity_Type :=
-                          Harriet.Installations.Required_Population
-                            (Installation);
+              Harriet.Installations.Required_Population
+                (Installation);
             Facility  : constant Harriet.Db.Facility.Facility_Type :=
-                          Harriet.Db.Facility.Get
-                            (Installation.Facility);
+              Harriet.Db.Facility.Get
+                (Installation.Facility);
             Power     : constant Natural := Facility.Power;
          begin
             Installations.Append
@@ -264,6 +155,11 @@ package body Harriet.Colonies.Updates is
          & "; efficiency:"
          & Natural'Image (Natural (Efficiency * 100.0)) & "%");
 
+      Harriet.Db.Expense.Create
+        (Colony  => Update.Colony,
+         Date    => Harriet.Calendar.Clock,
+         Expense => Total_Cost);
+
       declare
          New_Cash : Money_Type;
       begin
@@ -283,7 +179,10 @@ package body Harriet.Colonies.Updates is
             & "; efficiency:"
             & Natural'Image (Natural (Efficiency * 100.0)) & "%");
 
-         Cash := New_Cash;
+         Harriet.Db.Faction.Update_Faction (Colony.Faction)
+           .Set_Cash (New_Cash)
+           .Done;
+
       end;
 
       if Total_Power > Available_Power then
@@ -304,6 +203,119 @@ package body Harriet.Colonies.Updates is
            .Done;
       end loop;
 
-   end Update_Installations;
+   end Installations_Update;
+
+   -----------------------
+   -- Population_Update --
+   -----------------------
+
+   procedure Population_Update
+     (Update  : Colony_Update'Class)
+   is
+      use Harriet.Real_Images;
+      Colony  : constant Harriet.Db.Colony.Colony_Type :=
+        Harriet.Db.Colony.Get (Update.Colony);
+      World   : constant Harriet.Db.World.World_Type :=
+        Harriet.Db.World.Get (Colony.World);
+      Faction : constant Harriet.Db.Faction.Faction_Type :=
+        Harriet.Db.Faction.Get (Colony.Faction);
+      Birth_Rate  : constant Unit_Real :=
+                      0.02 * World.Habitability;
+      Death_Rate  : constant Unit_Real :=
+                      (2.0 - World.Habitability) / 200.0;
+      Daily_Rate  : constant Unit_Real :=
+                      (Birth_Rate - Death_Rate) / 365.0;
+      Current_Pop : constant Non_Negative_Real :=
+                      Harriet.Quantities.To_Real (Colony.Population);
+      New_Pop     : constant Non_Negative_Real :=
+                      Current_Pop * (1.0 + Daily_Rate);
+   begin
+      Harriet.Logging.Log
+        (World.Name
+         & " owned by "
+         & Faction.Name,
+         "habitability: "
+         & Approximate_Image (World.Habitability * 100.0) & "%"
+         & "; current pop "
+         & Approximate_Image (Current_Pop)
+         & "; birth rate: "
+         & Approximate_Image (Birth_Rate * 100.0) & "%"
+         & "; death rate: "
+         & Approximate_Image (Death_Rate * 100.0) & "%"
+         & "; change: "
+         & Approximate_Image (New_Pop - Current_Pop)
+         & "; new pop: "
+         & Approximate_Image (New_Pop));
+
+      Harriet.Db.Colony.Update_Colony (Update.Colony)
+        .Set_Population (Harriet.Quantities.To_Quantity (New_Pop))
+        .Done;
+
+   end Population_Update;
+
+   -----------------
+   -- Tax_Revenue --
+   -----------------
+
+   procedure Tax_Revenue
+     (Update  : Colony_Update'Class)
+   is
+      use Harriet.Elementary_Functions;
+      use Harriet.Money;
+      use Harriet.Real_Images;
+
+      Colony  : constant Harriet.Db.Colony.Colony_Type :=
+        Harriet.Db.Colony.Get (Update.Colony);
+      World   : constant Harriet.Db.World.World_Type :=
+        Harriet.Db.World.Get (Colony.World);
+      Faction : constant Harriet.Db.Faction.Faction_Type :=
+        Harriet.Db.Faction.Get (Colony.Faction);
+
+      Current_Wealth : constant Money_Type := Colony.Wealth;
+      Tax_Rate       : constant Unit_Real := Colony.Tax_Rate;
+      Tax_Evasion    : constant Unit_Real := Colony.Tax_Evasion;
+      New_Tax_Evasion : constant Unit_Real :=
+        0.9 * Colony.Tax_Evasion + 0.1 * Colony.Tax_Rate ** 2;
+      Loyalty         : constant Unit_Real := Colony.Loyalty;
+      New_Loyalty     : constant Unit_Real :=
+        0.9 * Colony.Loyalty + 0.1 * Sqrt (1.0 - Colony.Tax_Rate);
+      Daily_Rate     : constant Unit_Real :=
+                         (Tax_Rate * (1.0 - Tax_Evasion) * Sqrt (Loyalty))
+                         / 365.0;
+      Revenue        : constant Money_Type :=
+                         Adjust (Current_Wealth, Daily_Rate);
+   begin
+      Harriet.Logging.Log
+        (World.Name
+         & " owned by "
+         & Faction.Name,
+         "wealth: " & Show (Current_Wealth)
+         & "; tax rate: "
+         & Approximate_Image (Tax_Rate * 100.0) & "%"
+         & "; tax evasion: "
+         & Approximate_Image (Tax_Evasion * 100.0) & "%"
+         & "; loyalty: "
+         & Approximate_Image (Loyalty * 100.0) & "%"
+         & "; daily rate: "
+         & Approximate_Image (Tax_Rate * 100.0) & "%"
+         & "; revenue: "
+         & Show (Revenue));
+
+      Harriet.Db.Colony.Update_Colony (Update.Colony)
+        .Set_Wealth (Colony.Wealth - Revenue)
+        .Set_Tax_Evasion (New_Tax_Evasion)
+        .Set_Loyalty (New_Loyalty)
+        .Done;
+
+      Harriet.Db.Faction.Update_Faction (Colony.Faction)
+        .Set_Cash (Faction.Cash + Revenue)
+        .Done;
+
+      Harriet.Db.Revenue.Create
+        (Colony  => Update.Colony,
+         Date    => Harriet.Calendar.Clock,
+         Revenue => Revenue);
+
+   end Tax_Revenue;
 
 end Harriet.Colonies.Updates;
