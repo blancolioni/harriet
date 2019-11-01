48262239ce20d0512d1384b6ae4063ed17186103
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-11-01 07:20:50 +0100

    Harriet.Models.Star_System: using custom model instead of data source

--- src/ui/models/harriet-ui-models-star_system.adb
+++ src/ui/models/harriet-ui-models-star_system.adb
@@ -1,31 +1,29 @@
-with Ada.Text_IO;
+with Ada.Exceptions;
 
 with Harriet.Calendar;
-with Harriet.Quantities;
-
-with Harriet.Constants;
+with Harriet.Color;
+with Harriet.Orbits;
 with Harriet.Solar_System;
 
 with Harriet.Factions;
-with Harriet.Star_Systems;
+with Harriet.Stars;
 
 with Harriet.UI.Models.Data_Source;
-with Harriet.UI.Models.Values;
 
-with Harriet.Db.Colony;
+with Harriet.Db.Massive_Object;
+with Harriet.Db.Star;
 with Harriet.Db.Star_System;
+with Harriet.Db.Star_System_Object;
 with Harriet.Db.World;
 
 package body Harriet.UI.Models.Star_System is
 
    type Star_System_Model_Type is
-     new Harriet.UI.Models.Data_Source.Simple_Data_Source_Model with
+     new Root_Harriet_Model with
       record
-         Last_Fetch : Harriet.Calendar.Time;
+         Star_System : Harriet.Db.Star_System_Reference :=
+           Harriet.Db.Null_Star_System_Reference;
       end record;
-
-   overriding procedure On_Fetched
-     (Model : in out Star_System_Model_Type);
 
    overriding procedure Start
      (Model     : in out Star_System_Model_Type;
@@ -35,16 +33,30 @@ package body Harriet.UI.Models.Star_System is
    overriding function Name
      (Model : Star_System_Model_Type)
       return String
-   is ("star_system");
+   is ("star-system");
 
    overriding function Default_View_Name
      (Model : Star_System_Model_Type)
       return String
-   is ("Table");
+   is ("System");
 
    overriding function Changed
      (Model : Star_System_Model_Type)
       return Boolean;
+
+   overriding function Handle
+     (Model   : in out Star_System_Model_Type;
+      State   : in out State_Interface'Class;
+      Client  : Client_Id;
+      Request : Harriet.Json.Json_Value'Class)
+      return Harriet.Json.Json_Value'Class;
+
+   overriding function Get
+     (Model   : Star_System_Model_Type;
+      State   : State_Interface'Class;
+      Client  : Client_Id;
+      Request : Harriet.Json.Json_Value'Class)
+      return Harriet.Json.Json_Value'Class;
 
    -------------
    -- Changed --
@@ -54,26 +66,193 @@ package body Harriet.UI.Models.Star_System is
      (Model : Star_System_Model_Type)
       return Boolean
    is
-      use type Harriet.Calendar.Time;
+      pragma Unreferenced (Model);
    begin
-      Ada.Text_IO.Put_Line
-        ("star-system: last update "
-         & Harriet.Calendar.Image (Model.Last_Fetch, True)
-         & "; current time "
-         & Harriet.Calendar.Image (Harriet.Calendar.Clock, True));
-      return Model.Last_Fetch /= Harriet.Calendar.Clock;
+      return True;
    end Changed;
 
-   ----------------
-   -- On_Fetched --
-   ----------------
+   ---------
+   -- Get --
+   ---------
 
-   overriding procedure On_Fetched
-     (Model : in out Star_System_Model_Type)
+   overriding function Get
+     (Model   : Star_System_Model_Type;
+      State   : State_Interface'Class;
+      Client  : Client_Id;
+      Request : Harriet.Json.Json_Value'Class)
+      return Harriet.Json.Json_Value'Class
+   is
+      pragma Unreferenced (State, Client, Request);
+
+      function Orbiting_Objects
+        (Primary : Harriet.Db.Star_System_Object_Reference)
+         return Json.Json_Array;
+
+      function To_Json
+        (Object : Harriet.Db.Star_System_Object.Star_System_Object_Type)
+         return Json.Json_Object;
+
+      ----------------------
+      -- Orbiting_Objects --
+      ----------------------
+
+      function Orbiting_Objects
+        (Primary : Harriet.Db.Star_System_Object_Reference)
+         return Json.Json_Array
+      is
+      begin
+         return Arr : Json.Json_Array do
+            for Item of
+              Harriet.Db.Star_System_Object.Select_By_Primary
+                (Primary)
+            loop
+               Arr.Append (To_Json (Item));
+            end loop;
+         end return;
+      end Orbiting_Objects;
+
+      -------------
+      -- To_Json --
+      -------------
+
+      function To_Json
+        (Object : Harriet.Db.Star_System_Object.Star_System_Object_Type)
+         return Json.Json_Object
+      is
+         use Harriet.Calendar;
+         use Harriet.Db;
+
+         Result : Json.Json_Object;
+
+         procedure Set (Property_Name, Property_Value : String);
+         procedure Set (Property_Name : String;
+                        Property_Value : Real);
+         procedure Set (Property_Name  : String;
+                        Property_Value : Json.Json_Value'Class);
+
+         ---------
+         -- Set --
+         ---------
+
+         procedure Set (Property_Name, Property_Value : String) is
+         begin
+            Result.Set_Property (Property_Name, Property_Value);
+         end Set;
+
+         ---------
+         -- Set --
+         ---------
+
+         procedure Set (Property_Name  : String;
+                        Property_Value : Real)
+         is
+         begin
+            Result.Set_Property (Property_Name, Float (Property_Value));
+         end Set;
+
+         ---------
+         -- Set --
+         ---------
+
+         procedure Set (Property_Name  : String;
+                        Property_Value : Json.Json_Value'Class)
+         is
+         begin
+            Result.Set_Property (Property_Name, Property_Value);
+         end Set;
+
+      begin
+
+         Set ("name", Object.Name);
+         Set ("orbit",
+              Object.Semimajor_Axis / Harriet.Solar_System.Earth_Orbit);
+         Set ("longitude",
+              (if Object.Primary_Massive = Null_Massive_Object_Reference
+               then 0.0
+               else Harriet.Orbits.Calculate_Longitude
+                 (Massive_Object.Get (Object.Primary_Massive).Mass,
+                  Object.Semimajor_Axis,
+                  Harriet.Calendar.Clock - Object.Epoch)));
+         Set ("day", Object.Period);
+         Set ("axisTilt", Object.Tilt);
+         Set ("dependents",
+              Orbiting_Objects (Object.Get_Star_System_Object_Reference));
+
+         if Object.Top_Record = R_Star then
+            declare
+               Star : constant Harriet.Stars.Star_Type'Class :=
+                 Harriet.Stars.Get
+                   (Harriet.Db.Star.Get_Star
+                      (Object.Get_Star_System_Object_Reference));
+               Color : constant Harriet.Color.Harriet_Color :=
+                 Star.Color;
+            begin
+               Set ("mass", Object.Mass / Harriet.Solar_System.Solar_Mass);
+               Set ("radius",
+                    Object.Radius / Harriet.Solar_System.Solar_Radius);
+               Set ("temperature", Star.Temperature);
+               Set ("red", Color.Red);
+               Set ("green", Color.Green);
+               Set ("blue", Color.Blue);
+            end;
+         elsif Object.Top_Record = R_World then
+            declare
+               World  : constant Harriet.Db.World.World_Type :=
+                 Harriet.Db.World.Get_World
+                   (Object.Get_Star_System_Object_Reference);
+            begin
+               Set ("mass", Object.Mass / Harriet.Solar_System.Earth_Mass);
+               Set ("radius",
+                    Object.Radius / Harriet.Solar_System.Earth_Radius);
+               Set ("temperature", World.Average_Temperature);
+               Set ("composition",
+                    Harriet.Db.World_Composition'Image
+                      (World.Composition));
+               Set ("climate",
+                    Harriet.Db.World_Climate'Image
+                      (World.Climate));
+            end;
+
+         end if;
+
+         return Result;
+
+      exception
+         when E : others =>
+            raise Constraint_Error with
+              "exception while serializing "
+              & Record_Type'Image (Object.Top_Record)
+              & " "
+              & Object.Name
+              & ": "
+              & Ada.Exceptions.Exception_Message (E);
+      end To_Json;
+
+      Result : Json.Json_Object;
+
+   begin
+      Result.Set_Property
+        ("systemName", Harriet.Db.Star_System.Get (Model.Star_System).Name);
+      Result.Set_Property
+        ("primary",
+         To_Json (Harriet.Db.Star.First_By_Star_System (Model.Star_System)));
+      return Result;
+   end Get;
+
+   ------------
+   -- Handle --
+   ------------
+
+   overriding function Handle
+     (Model   : in out Star_System_Model_Type;
+      State   : in out State_Interface'Class;
+      Client  : Client_Id;
+      Request : Harriet.Json.Json_Value'Class)
+      return Harriet.Json.Json_Value'Class
    is
    begin
-      Model.Last_Fetch := Harriet.Calendar.Clock;
-   end On_Fetched;
+      return Model.Get (State, Client, Request);
+   end Handle;
 
    -----------------------
    -- Star_System_Model --
@@ -81,10 +260,7 @@ package body Harriet.UI.Models.Star_System is
 
    function Star_System_Model return Root_Harriet_Model'Class is
    begin
-      return Model : constant Star_System_Model_Type :=
-        Star_System_Model_Type'
-          (Harriet.UI.Models.Data_Source.Simple_Data_Source_Model with
-             Last_Fetch => Harriet.Calendar.Clock);
+      return Model : Star_System_Model_Type;
    end Star_System_Model;
 
    -----------
@@ -99,122 +275,13 @@ package body Harriet.UI.Models.Star_System is
       Faction  : constant Harriet.Factions.Faction_Type'Class :=
         Harriet.Factions.Get_User_Faction (User);
 
-      Capital  : constant Harriet.Db.Star_System_Reference :=
-        (if Faction.Has_Element
-         then Faction.Capital_System
-         else Harriet.Db.Null_Star_System_Reference);
-
-      Name     : constant String :=
-        (if Arguments = ""
-         then Harriet.Star_Systems.Name (Capital)
-         else Arguments);
-
-      Star_System : constant Harriet.Db.Star_System_Reference :=
-        Harriet.Db.Star_System.First_Reference_By_Name (Name);
-
    begin
-      Model.Add_Column
-        (Id       => "name",
-         Label    => "Name",
-         Col_Type => Values.Text_Type);
-      Model.Add_Column
-        (Id       => "orbit",
-         Label    => "Orbit (AU)",
-         Col_Type => Values.Real_Type);
-      Model.Add_Column
-        (Id       => "radius",
-         Label    => "Radius (R"
-         & Character'Val (16#E2#)
-         & Character'Val (16#82#)
-         & Character'Val (16#91#)
-         & ")",
-         Col_Type => Values.Real_Type);
-      Model.Add_Column
-        (Id => "gravity",
-         Label => "g",
-         Col_Type => Values.Real_Type);
-      Model.Add_Column
-        (Id       => "surfaceTemperature",
-         Label    =>
-           "Suface Temp ("
-         & Character'Val (16#C2#)
-         & Character'Val (16#B0#)
-         & "C)",
-         Col_Type => Values.Real_Type);
-      Model.Add_Column
-        (Id       => "surfacePressure",
-         Label    => "Surface Pressure (atm)",
-        Col_Type => Values.Real_Type);
-      Model.Add_Column
-        (Id       => "category",
-         Label    => "Category",
-         Col_Type => Values.Text_Type);
-      Model.Add_Column
-        (Id       => "climate",
-         Label    => "Climate",
-         Col_Type => Values.Text_Type);
-      Model.Add_Column
-        (Id       => "population",
-         Label    => "Population",
-         Col_Type => Values.Real_Type);
-
-      declare
-
-         procedure Add_Row
-           (World : Harriet.Db.World.World_Type);
-
-         -------------
-         -- Add_Row --
-         -------------
-
-         procedure Add_Row
-           (World : Harriet.Db.World.World_Type)
-         is
-            use Harriet.Constants, Harriet.Solar_System;
-            --              use Harriet.Db;
-
-            function T (S : String) return Values.Model_Value_Type
-                        renames Values.Text_Value;
-            function R (X : Real) return Values.Model_Value_Type
-                        renames Values.Real_Value;
-            function Q
-              (X : Quantities.Quantity_Type)
-               return Values.Model_Value_Type
-            is (Values.Real_Value (Quantities.To_Real (X)));
-
-            Colony : constant Harriet.Db.Colony.Colony_Type :=
-              Harriet.Db.Colony.Get_By_World (World.Get_World_Reference);
-
-         begin
-            Model.Add_Row
-              ((T (World.Name),
-               R (World.Semimajor_Axis / Harriet.Solar_System.Earth_Orbit),
-               R (World.Radius / Harriet.Solar_System.Earth_Radius),
-               R (World.Surface_Gravity),
-               R (World.Average_Temperature - Freezing_Point_Of_Water),
---                 R (World.Minimum_Temperature - Freezing_Point_Of_Water),
---                 R (World.Maximum_Temperature - Freezing_Point_Of_Water),
---                 R (World.Night_Temperature_Low - Freezing_Point_Of_Water),
---               R (World.Daytime_Temperature_High - Freezing_Point_Of_Water),
-               (if World.Gas_Giant
-                then R (0.0)
-                else R (World.Surface_Pressure / Earth_Surface_Pressure)),
-               T (Harriet.Db.World_Composition'Image (World.Composition)),
-               T (Harriet.Db.World_Climate'Image (World.Climate)),
-               Q ((if Colony.Has_Element
-                 then Colony.Population else Quantities.Zero))));
-         end Add_Row;
-
-      begin
-         for World of
-           Harriet.Db.World.Select_By_Star_System (Star_System)
-         loop
-            Add_Row (World);
-         end loop;
-      end;
-
-      Model.Last_Fetch := Harriet.Calendar.Clock;
-
+      if Arguments = "" then
+         Model.Star_System := Faction.Capital_System;
+      else
+         Model.Star_System :=
+           Harriet.Db.Star_System.First_Reference_By_Name (Arguments);
+      end if;
    end Start;
 
 end Harriet.UI.Models.Star_System;
