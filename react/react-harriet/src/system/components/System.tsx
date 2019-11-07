0a4460357039d6a5e094d1a6f53d3b49aaaa46a6
Fraser Wilson <fraser@thewhitelion.eu>
2019-11-07 20:35:40 +0100

    Fixed ship scale.  Save current zoomed object, show distance

--- react/react-harriet/src/system/components/System.tsx
+++ react/react-harriet/src/system/components/System.tsx
@@ -16,6 +16,10 @@ interface Props {
     clientDispatch : Dispatch
 }
 
+interface ObjectTable {
+  [key : string] : SystemObject
+}
+
 class Component extends React.Component<Props,State> {
 
   mount: any;
@@ -115,8 +119,11 @@ class Component extends React.Component<Props,State> {
   model  : Model3D | null;
   renderCount : number
   unTimeMaterial : THREE.ShaderMaterial[];
-  currentZoom : THREE.Mesh | null;
+  currentZoom : THREE.Mesh | null = null;
+  currentLabel : CSS2DObject | null = null;
+  zoomObject : SystemObject | null = null;
   localLight : THREE.Light | null = null;
+  objects : ObjectTable = {};
 
   constructor(props : Props) {
     super (props);
@@ -124,7 +131,6 @@ class Component extends React.Component<Props,State> {
     this.renderCount = 0;
     this.unTimeMaterial = [];
     this.model = null;
-    this.currentZoom = null;
     this.beforeRender = this.beforeRender.bind(this);
   }
 
@@ -160,16 +166,33 @@ class Component extends React.Component<Props,State> {
     return mesh;
   }
 
+  showDistance = (d : number) : string => {
+    if (d < 1000) {
+      return Math.floor(d) + 'm';
+    } else if (d < 1e9) {
+      return Math.floor(d / 1000) + 'km';
+    } else {
+      return Math.floor(d / 1.5e8) / 1000 + 'AU';
+    }
+  }
+
   beforeRender() {
     for (const mat of this.unTimeMaterial) {
       mat.uniforms.unTime.value = this.renderCount;
+    }
+    if (this.currentLabel) {
+      const v1 = this.model!.camera.position;
+      const v2 = this.currentZoom!.position;
+      const d = v1.distanceTo(v2);
+      const img = this.showDistance(d);
+
+      this.currentLabel.element.textContent = this.zoomObject!.name + ' ' + img;
     }
     
     this.renderCount += 0.0002;
   }
 
-  addObject = (obj : SystemObject, mesh : THREE.Mesh) => {
-    let scale = obj.radius;    
+  addObject = (obj : SystemObject, mesh : THREE.Mesh, scale : number = 1) => {
     mesh.scale.set(scale, scale, scale);
     mesh.name = obj.id;
     this.model!.scene.add( mesh );
@@ -180,12 +203,15 @@ class Component extends React.Component<Props,State> {
     labelDiv.style.marginTop = '-1em';
     var labelObject = new CSS2DObject( labelDiv );
     labelObject.position.set( 0.5, -0.1, 0 );
+    labelObject.name = mesh.name + '-label';
     mesh.add( labelObject );
     const mat = mesh.material;
 
     if ('uniforms' in mat) {
       this.unTimeMaterial.push(mat);
     }
+
+    this.objects[obj.id] = obj;
 
 
   }
@@ -200,11 +226,11 @@ class Component extends React.Component<Props,State> {
       newObject = true;
       switch (obj.type) {
         case SystemObjectType.Star:
-          this.addObject(obj, this.starMesh(obj as StarObject))
+          this.addObject(obj, this.starMesh(obj as StarObject), obj.radius)
           break;
 
         case SystemObjectType.World:
-          this.addObject(obj, worldMesh(this.model!, obj as WorldObject, 3, new THREE.Vector3(-x, -y, -z)));
+          this.addObject(obj, worldMesh(this.model!, obj as WorldObject, 3, new THREE.Vector3(-x, -y, -z)), obj.radius);
           break;
 
         case SystemObjectType.Ship:
@@ -232,10 +258,12 @@ class Component extends React.Component<Props,State> {
       const newZoom = this.model!.scene.getObjectByName(this.props.clientState.zoom);
       if (newZoom) {
         this.currentZoom = newZoom as THREE.Mesh;
+        this.currentLabel = (newZoom.getObjectByName(newZoom.name + '-label') as CSS2DObject);
+        this.zoomObject = this.objects[this.props.clientState.zoom];
         const mesh = this.currentZoom;
         const { x, y, z } = mesh.position;
-        const d = mesh.scale.z;
-        const n = new THREE.Vector3(x, y, z).normalize().multiplyScalar(d * 20);
+        const d = this.objects[this.props.clientState.zoom].radius;
+        const n = new THREE.Vector3(x, y, z).normalize().multiplyScalar(d * 5);
         const wp = new THREE.Vector3 (x, y, z).sub(n);
         const lt = mesh.position.clone().normalize();
         this.localLight!.position.set(-lt.x, -lt.y, -lt.z);
